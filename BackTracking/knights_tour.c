c97539aa812244a6e4d9b2f24a0e26c6fc6ed90f
abhjain4 <abhishek50000@gmail.com>
2016-12-14 16:52:31 -0800

    Knights tour quickest

--- BackTracking/knights_tour.c
+++ BackTracking/knights_tour.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
+#define N 8
 /* Matrix to store the visited blocks */
-bool chess_board[8][8] = {false};
+bool chess_board[N][N] = {false};
 
 /* Recursive function to calculate each move */
 bool move(int x, int y, int count){
     /* If out of bounds return false straight away */
-    //printf("[%d,%d], %d\n",x,y,count);
-    if( x > 7 || y > 7 || x < 0 || y < 0){
+    if( x > N-1 || y > N-1 || x < 0 || y < 0){
         return false;
     }
 
@@ -16,22 +16,23 @@ bool move(int x, int y, int count){
         chess_board[x][y] = true;
         count ++;
 
-        if((count == 63) || 
-                 move (x+2, y-1, count) ||
-                 move (x+2, y+1, count) ||
-                 move (x-2, y-1, count) ||
-                 move (x-2, y+1, count) ||
-                 move (x+1, y+2, count) ||
-                 move (x-1, y+2, count) ||
-                 move (x+1, y-2, count) ||
-                 move (x-1, y-2, count)
-                ){
+        if((count == N*N) || 
+               move (x+2, y+1, count) ||
+               move (x+1, y+2, count) ||
+               move (x-1, y+2, count) ||
+               move (x-2, y+1, count) ||
+               move (x-2, y-1, count) ||
+               move (x-1, y-2, count) ||
+               move (x+1, y-2, count) ||
+               move (x+2, y-1, count)
+              ){
             /* If all boxes visted return true */
                     /* OR */
             /*either of the 8 moves returns true*/
-            printf("%d ",(x + y*8));
+            printf("%d ",(x + y*N));
             return true;
         }else{
+            count --;
             chess_board[x][y] = false;
             return false;
         }
@@ -43,5 +44,7 @@ bool move(int x, int y, int count){
 
 
 int main(){
-    move(0,0,0);
+    if(!move(0,0,0)){
+        printf ("Sorry no result found: Try different starting co-ordinates\n");
+    }
 }
